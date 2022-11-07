8aeff9b783e3d3bd568c96cfbfc4b69065ea862f
reisrobson12@gmail.com <reisrobson12@gmail.com>
2022-11-07 20:49:10 -0300

    update file

--- cp1.js
+++ cp1.js
@@ -1,60 +1,15 @@
-let comida = "macarrão"; 
-
-let tempo = 10
-
-switch(comida){
-    case "pipoca":
-       if (tempo > 20 && tempo <= 29) {
-        console.log (comida = "comida queimou");
-    } else if ("pipoca" <10){
-        console.log ("tempo insuficiente")
-    } else if ("pipoca" >= 30) {
-        comida = "kabumm";
-    } else { console.log ("Prato pronto, bom apetite!!!")}
-        break;
-
-    case "macarrão":
-        if ("macarrão" > 8 && "macarrão" <= 23) {
-        console.log ("comida queimou");
-    } else if ("macarrão" <8){
-        console.log ("tempo insuficiente");
-    } else if ("macarrão" >= 24) {
-        console.log ("kabumm");
-    } else { console.log ("Prato pronto, bom apetite!!!")}
-        break;
-
-    case "carne":
-        if ("carne" > 30 && "carne" <= 44) {
-        console.log ("comida queimou");
-    } else if ("carne" <15){
-        console.log ("tempo insuficiente");
-    } else if ("carne" >= 45) {
-        console.log ("kabumm");
-    } else { console.log ("Prato pronto, bom apetite!!!")}
-        break;
-
-    case "Feijão":
-        if ("Feijão" > 24 && "Feijão" <= 35) {
-        console.log ("comida queimou");
-    } else if ("Feijão" <12){
-        console.log ("tempo insuficiente");
-    } else if ("Feijão" >= 36) {
-        console.log ("kabumm");
-    } else { console.log ("Prato pronto, bom apetite!!!")}
-        break;
-
-    case "Brigadeiro":
-        if ("Brigadeiro" > 16 && "Brigadeiro" <= 23) {
-        console.log ("comida queimou");
-    } else if ("Brigadeiro" <8){
-        console.log ("tempo insuficiente");
-    } else if ("Brigadeiro" >= 24) {
-        console.log ("kabumm");
-    } else { console.log ("Prato pronto, bom apetite!!!")}
-        break;
-
-    default: 
-        console.log ("Prato inexistente")
-    
-}
-
+function microondas(tempo, comida) {
+    if (comida === 'pipoca') {
+      if (tempo > 20 && tempo <= 29) {
+        return 'comida queimou'
+      } else if (tempo < 10) {
+        return 'tempo insuficiente'
+      } else if (tempo >= 30) {
+        return 'kabumm'
+      } 
+    }
+  }
+  
+  console.log(microondas(1, 'pipoca'))
+  console.log(microondas(35, 'pipoca'))
+  console.log(microondas(25, 'pipoca'))
